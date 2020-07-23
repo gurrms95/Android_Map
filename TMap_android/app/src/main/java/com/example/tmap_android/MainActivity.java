dab9c46623ab85dbe026c3627c49709514cbd4b8
gurrms95 <gurrms95@gmail.com>
2020-07-23 16:51:26 +0900

    Tmap_andfoir

--- TMap_android/app/src/main/java/com/example/tmap_android/MainActivity.java
+++ TMap_android/app/src/main/java/com/example/tmap_android/MainActivity.java
@@ -8,9 +8,12 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.skt.Tmap.TMapData;
@@ -21,8 +24,7 @@ import com.skt.Tmap.TMapView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements TMapGpsManager.onLocationChangedCallback {
-
+public class MainActivity extends AppCompatActivity{
     private Context mContext = null;
     private  boolean m_bTrackingMode = true;
 
@@ -30,17 +32,15 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
     private TMapView tmapView = null;
     private static  String mApiKey = "l7xxf24764cb422249989a969b9dc2b55533";
     private static  int mMarkerID;
+    private TMapGpsManager gps;
+    private LocationListener locationListener;
 
     private ArrayList<TMapPoint> m_tmapPoint = new ArrayList<TMapPoint>();
     private ArrayList<String> mArrayMarkerID = new ArrayList<String>();
     private  ArrayList<MapPoint> m_mapPoint = new ArrayList<MapPoint>();
 
-    @Override
-    public void onLocationChange(Location location) {
-        double lat = location.getLatitude();
-        double lon = location.getLongitude();
-        TMapPoint point = tmapgps.getLocation();
-    }
+    private Button onLast_button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,16 +56,94 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
         tmapView.setSKTMapApiKey(mApiKey);
         tmapView.setIconVisibility(true);
 
-        TMapGpsManager gps = new TMapGpsManager(this);
-        gps.setMinTime(1000);
-        gps.setMinDistance(5);
-        gps.setProvider(gps.GPS_PROVIDER);
+        gps = new TMapGpsManager(this);
+//        gps.setMinTime(30);
+//        gps.setMinDistance(5);
+//        gps.setProvider(gps.GPS_PROVIDER);
+
+//        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED) {
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1); //위치권한 탐색 허용 관련 내용
+//            }
+//            return;
+//        }
+//
+//
+//        LocationManager locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+//        locationListener = new LocationListener() {
+//            @Override
+//            public void onLocationChanged(Location location) {
+//                if(location != null) {
+//                    double latitude = location.getLatitude();
+//                    double longitude = location.getLongitude();
+//
+//                    tmapView.setCenterPoint(longitude, latitude);
+//                    tmapView.setLocationPoint(longitude, latitude);
+//                }
+//            }
+//
+//            @Override
+//            public void onStatusChanged(String provider, int status, Bundle extras) {}
+//
+//            @Override
+//            public void onProviderEnabled(String provider) {}
+//
+//            @Override
+//            public void onProviderDisabled(String provider) {}
+//        };
+//
+//        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+//        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+//        tmapView.setIconVisibility(true);
+//
+//        gps.OpenGps();
+//
+//        tmapView.setTrackingMode(true);
+//        tmapView.setSightVisible(true);
+
+
+
+    }
+
+
+    public void onLastLocationButtonClicked(View view) {
+
+//        gps.setMinTime(30);
+//        gps.setMinDistance(5);
+//        gps.setProvider(gps.GPS_PROVIDER);
+
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1); //위치권한 탐색 허용 관련 내용
             }
             return;
         }
+
+        LocationManager locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if(location != null) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+
+                    tmapView.setCenterPoint(longitude, latitude);
+                    tmapView.setLocationPoint(longitude, latitude);
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            @Override
+            public void onProviderEnabled(String provider) {}
+
+            @Override
+            public void onProviderDisabled(String provider) {}
+        };
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
         tmapView.setIconVisibility(true);
 
         gps.OpenGps();
@@ -73,25 +151,5 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
         tmapView.setTrackingMode(true);
         tmapView.setSightVisible(true);
 
-
-    }
-
-    public void onLastLocationButtonClicked(View view) {
-//        TMapGpsManager gps = new TMapGpsManager(this);
-//        gps.setMinTime(1000);
-//        gps.setMinDistance(5);
-//        gps.setProvider(gps.GPS_PROVIDER);
-//        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED) {
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1); //위치권한 탐색 허용 관련 내용
-//            }
-//            return;
-//        }
-//        tmapView.setIconVisibility(true);
-//
-//        gps.OpenGps();
-//
-//        tmapView.setTrackingMode(true);
-//        tmapView.setSightVisible(true);
     }
 }
